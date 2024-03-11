cade2c06daccb1eadb03391c07de0ff1c30697c8
Alexandre FullStack Dev <138938681+GunTsy@users.noreply.github.com>
2024-03-10 22:54:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const User = require('./models/User');
+
+app.use(express.json());
+
+const db =require('./models/db.js')
+
+app.get("/", async (req, res) => {
+    res.send("Página inicial - Alexandre")
+});
+
+app.post("/cadastrar", async (req, res) => {
+    // console.log(req.body);
+
+    await User.create(req.body)
+        .then(() => {
+            return res.json ({
+                erro: false,
+                mensagem: "Usuario cadastrado com sucesso!"
+            });
+        }).catch(() =>  {
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Usuário não cadastrado!"
+            });
+        });
+
+    res.send("Página cadastrar")
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080")
+})
